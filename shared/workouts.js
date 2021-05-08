559f82db421e579a449bedf077b7b511e9e4fa3f
Saad Zafar <saadzafar@saads-air.lan>
2021-05-08 17:46:19 -0400

    Scaffold

---
+++ shared/workouts.js
@@ -0,0 +1,34 @@
+export const WORKOUTS = [
+    {
+        id: 0,
+        name: "Chest and Triceps",
+        image: "/assets/images/react-lake.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
+    },
+    {
+        id: 1,
+        name:"Back and Biceps",
+        image: "/assets/images/chrome-river.jpg",
+        elevation: 877,
+        featured: false,
+        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+    },
+    {
+        id: 2,
+        name:"Shoulders and Abs",
+        image: "/assets/images/breadcrumb-trail.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
+    },
+    {
+        id: 3,
+        name:"Legs",
+        image: "/assets/images/redux-woods.jpg",
+        elevation: 42,
+        featured: true,
+        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
+];
